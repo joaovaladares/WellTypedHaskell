3f123a082eb89e97d7c125bf67ed9cea186d7950
Joao Valadares <joaovvaladares1@gmail.com>
2024-12-20 20:57:46 -0300

    Foldr = standard design pattern on lists

---
+++ Folds.hs
@@ -0,0 +1,39 @@
+module Folds where
+
+import Prelude hiding (elem, map, and, or, filter)
+
+elem :: Eq a => a -> [a] -> Bool
+elem _ []       = False
+elem y (x : xs) = y == x || elem y xs
+
+map :: (a -> b) -> [a] -> [b]
+map _ []       = []
+map f (x : xs) = f x : map f xs
+
+and :: [Bool] -> Bool
+and []       = True
+and (x : xs) = x && and xs
+
+-- same as foldr (but less general since foldr works on any Foldable)
+fn :: (a -> r -> r) -> r -> [a] -> r
+fn cons nil []       = nil
+fn cons nil (x : xs) = cons x (fn cons nil xs)
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' y = fn (\ x r -> x == y || r) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = fn (\ x r -> f x : r ) []
+
+and' :: [Bool] -> Bool
+and' = fn (&&) True
+
+or' :: [Bool] -> Bool
+or' = foldr (||) False 
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f = foldr (\ x r -> if f x then x : r else r) []
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' f = foldr (\ x r -> f x || r) False
+
